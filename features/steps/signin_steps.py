5b5a431c66f54e57b72d01cd5395cbf4a080bb38
Leanna T <Lgodiva0808@gmail.com>
2025-09-12 16:27:56 -0400

    HM3_Task3

---
+++ features/steps/signin_steps.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+
+@given('Open Target main page')
+def open_main(context):
+    context.driver.get('https://www.target.com/')
+
+@when('Click Account button')
+def click_account_button(context):
+    context.driver.find_element(By.XPATH, "//*[@data-test='@web/AccountLink']").click()
+    sleep(2)
+
+@when('Click Sign In button')
+def click_sign_in_button(context):
+    context.driver.find_element(By.XPATH, "//*[@data-test='accountNav-signIn']").click()
+    sleep(2)
+
+@then('SignIn page opened')
+def sign_in_page_opened(context):
+    expected = 'Sign in or create account'
+    actual = context.driver.find_element(By.XPATH, "//h1[contains(@class, 'styles_ndsHeading')]").text
+    assert expected == actual, f'Expected {expected} did not match {actual}'
---
+++ features/tests/target_signin.feature
@@ -0,0 +1,9 @@
+
+Feature: Tests for Target Sign In functionality (HW3Task3)
+
+
+  Scenario: User can Sign In on Target
+    Given Open Target main page
+    When Click Account button
+    When Click Sign In button
+    Then SignIn page opened
